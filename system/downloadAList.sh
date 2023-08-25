83550a0805ba34bf620bedc020f357fd0f1bb9d0
homura <943384135@qq.com>
2023-08-25 22:36:38 +0800

    build

--- system/downloadAList.sh
+++ system/downloadAList.sh
@@ -4,6 +4,7 @@ alist=".cache/alist-linux-amd64.tar.gz"
 
 if [ -e "$file_path" ]; then
     echo "File exists at $file_path."
+    tar -xvf .cache/alist-linux-amd64.tar.gz -C /var/www/app1/AList
 else
     mkdir /var/www/app1/AList
     wget -P .cache https://github.com/alist-org/alist/releases/download/v3.26.0/alist-linux-amd64.tar.gz 
